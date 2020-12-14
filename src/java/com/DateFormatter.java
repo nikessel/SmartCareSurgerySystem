a21400f4121f7d41c7cd019d1ee564273bd25057
niklass-l <71767810+niklass-l@users.noreply.github.com>
2020-12-14 10:52:09 +0000

    priv.xml

---
+++ src/java/com/DateFormatter.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com;
+
+/**
+ *
+ * @author niklas
+ */
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public final class DateFormatter {
+    
+    Date date;
+
+    private DateFormatter() {
+    }
+
+    public static String formatDate(Date date, String pattern) {
+        pattern = "dd-MM-yyyy";
+        return new SimpleDateFormat(pattern).format(date);
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+    
+    
+    
+}
